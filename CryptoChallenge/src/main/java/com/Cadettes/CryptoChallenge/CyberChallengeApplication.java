0b472c059d6e97d685ef1d8c2bb092e54bad26b9
aicha <mimouni.a@hotmail.fr>
2024-12-23 17:11:42 +0100

    ajout challenge CryptoChallenge

---
+++ CryptoChallenge/src/main/java/com/Cadettes/CryptoChallenge/CyberChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.Cadettes.CryptoChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberChallengeApplication.class, args);
+	}
+
+}
